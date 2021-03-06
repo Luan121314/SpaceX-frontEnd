dcda6d12966ab592f093527e494c55c79d71215b
luan.staner <luan.staner@gmail.com>
2021-03-06 18:07:23 -0300

    updated endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: "https://apispacex04.herokuapp.com"
-    baseURL: "http://localhost:3333"
+    baseURL: "https://apispacex04.herokuapp.com"
 })
 
 export default api;
